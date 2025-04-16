044a05db4aacbfcda5352a5248b7afcad6cae48f
jnswkz <23520408@gm.uit.edu.vn>
2025-04-16 09:07:59 +0700

    Connect Login with backend server and add check user account

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  cors: {
+    origin: 'http://localhost:8000',
+  },
   plugins: [react(), tailwindcss()],
 })
